0d504d8250d714d092f505d4088c9b36ee07ed46
ChrisAlex30 <akshara155301@gmail.com>
2025-08-05 17:43:17 +0530

    chore : tests done

---
+++ tests/counter.ts
@@ -0,0 +1,95 @@
+import * as anchor from "@coral-xyz/anchor";
+import {Program} from "@coral-xyz/anchor";
+import {Counter} from "../target/types/counter";
+import {expect} from "chai";
+
+describe("Counter Program",()=>{
+  const provider=anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program=anchor.workspace.Counter as Program<Counter>;
+
+  const user=provider.wallet;
+  let counterAccount=anchor.web3.Keypair.generate();
+
+  it("Initialize Counter",async()=>{
+      await program.methods.initialize().accounts({
+        user:user.publicKey,
+        counter:counterAccount.publicKey
+      }).signers([counterAccount]).rpc();
+
+      const counter=await program.account.counter.fetch(counterAccount.publicKey);
+      console.log({counter});
+
+      expect(counter.count).equal(0); 
+      
+    });
+
+  it("Increment the counter",async ()=>{
+    await program.methods.increment().accounts({
+      counter:counterAccount.publicKey
+    }).rpc();
+
+    const counter=await program.account.counter.fetch(counterAccount.publicKey);
+    console.log(counter);
+
+    expect(counter.count).equal(1);
+    
+  });
+
+  it("Decrement the counter",async ()=>{
+    await program.methods.decrement().accounts({
+      counter:counterAccount.publicKey
+    }).rpc();
+
+    const counter=await program.account.counter.fetch(counterAccount.publicKey);
+    console.log(counter);
+
+    expect(counter.count).equal(0);
+    
+  })
+
+  it("Adds to the counter",async ()=>{
+    await program.methods.add(10).accounts({
+      counter:counterAccount.publicKey
+    }).rpc();
+
+    const counter=await program.account.counter.fetch(counterAccount.publicKey);
+    console.log(counter);
+
+    expect(counter.count).equal(10);
+    
+  })
+
+  it("Set the counter",async ()=>{
+    await program.methods.set(50).accounts({
+      counter:counterAccount.publicKey
+    }).rpc();
+
+    const counter=await program.account.counter.fetch(counterAccount.publicKey);
+    console.log(counter);
+
+    expect(counter.count).equal(50);
+    
+  })
+
+  it("Closes the counter", async () => {
+  await program.methods
+    .close()
+    .accounts({
+      user: user.publicKey,
+      counter: counterAccount.publicKey,
+    })
+    .rpc();
+
+  try {
+    await program.account.counter.fetch(counterAccount.publicKey);
+    // If fetch doesn't throw, the test should fail
+    throw new Error("Counter account still exists after close.");
+  } catch (err) {
+    // This is the expected behavior
+    expect(err.message).to.contain("Account does not exist");
+  }
+});
+
+})
